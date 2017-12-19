ede0e6253badfe6d91359c2256cebbe7c6dfa9ed
Unknown <gabingirardeau44@gmail.com>
2017-12-19 15:34:04 +0100

    Ajout de la bordure, masquage de l'icone, insertion de l'icône dans le canvas

--- js/snake.js
+++ js/snake.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
+    insertIcon();
+    console.log("premier lancement");
+    function insertIcon() {
+        var canvas = $("#snakeBoard");
+        if(canvas.get(0)) {
+            var ctx = canvas.get(0).getContext("2d");
+            var img = $("#e_econocom");
+            if(img.get(0)) {
+                ctx.drawImage(img.get(0),10,10);
+            }
+        }
 
+    }
 });

2a92cfa8abf0d44bd5b0cbf7bf7c6b153eb8bf61
Unknown <gabingirardeau44@gmail.com>
2017-12-20 11:14:52 +0100

    titre + le e ne se duplique plus + delimitation des zones

--- js/snake.js
+++ js/snake.js
@@ -1,40 +1,49 @@
 $(document).ready(function () {
-    insertIcon();
+    var canvas = $("#snakeBoard");
     var ex = 10;
     var ey = 10;
+    var canvasWidth = 300;
+    var canvasHeight = 150;
+    insertIcon();
+    var titre = $("#titre");
+    console.log(canvas.css("width"));
+    console.log(canvas.css("height"));
+
+    $("body").keydown(function (event) {
+        var keyPress = event.which;
+        move(keyPress);
+        insertIcon();
+
+    });
 
     function insertIcon() {
-        var canvas = $("#snakeBoard");
         if (canvas.get(0)) {
             var ctx = canvas.get(0).getContext("2d");
+            ctx.clearRect(0, 0, canvasWidth, canvasHeight);
             var img = $("#e_econocom");
             if (img.get(0)) {
                 ctx.drawImage(img.get(0), ex, ey);
             }
         }
 
-    }
+    };
 
     function move(keyPress) {
-        if (keyPress == 37 && ex > 10) {
+        if (keyPress == 37 && ex > 0) {
             ex -= 1;
-        } else if (keyPress == 38 && ey > 10) {
+        } else if (keyPress == 38 && ey > 0) {
             ey -= 1.
-        } else if (keyPress == 39 && ex < 1200) {
+        } else if (keyPress == 39 && ex < (canvasWidth - 6)) {
             ex += 1
-        } else if (keyPress == 40 && ey < 800) {
+        } else if (keyPress == 40 && ey < (canvasHeight - 5)) {
             ey += 1
-        } else {
-            alert('stop');
         }
-        console.log('ex=' + ex + ' | ey=' + ey);
-    }
 
-    $("body").keydown(function (event) {
-        var keyPress = event.which;
-        move(keyPress);
-        console.log(keyPress);
-    });
+        // else {
+        //     alert('stop');
+        // }
+        console.log('ex=' + ex + ' | ey=' + ey);
+    };
 });
 
 

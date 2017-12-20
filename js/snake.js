c7a1e961f9128f73b773843fd2db9994c5f6bca7
Unknown <gabingirardeau44@gmail.com>
2017-12-20 09:15:47 +0100

    bloquer la serpent

--- js/snake.js
+++ js/snake.js
@@ -1,15 +1,40 @@
 $(document).ready(function () {
     insertIcon();
-    console.log("premier lancement");
+    var ex = 10;
+    var ey = 10;
+
     function insertIcon() {
         var canvas = $("#snakeBoard");
-        if(canvas.get(0)) {
+        if (canvas.get(0)) {
             var ctx = canvas.get(0).getContext("2d");
             var img = $("#e_econocom");
-            if(img.get(0)) {
-                ctx.drawImage(img.get(0),10,10);
+            if (img.get(0)) {
+                ctx.drawImage(img.get(0), ex, ey);
             }
         }
 
     }
+
+    function move(keyPress) {
+        if (keyPress == 37 && ex > 10) {
+            ex -= 1;
+        } else if (keyPress == 38 && ey > 10) {
+            ey -= 1.
+        } else if (keyPress == 39 && ex < 1200) {
+            ex += 1
+        } else if (keyPress == 40 && ey < 800) {
+            ey += 1
+        } else {
+            alert('stop');
+        }
+        console.log('ex=' + ex + ' | ey=' + ey);
+    }
+
+    $("body").keydown(function (event) {
+        var keyPress = event.which;
+        move(keyPress);
+        console.log(keyPress);
+    });
 });
+
+

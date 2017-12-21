5ed13df421a5edd3d8ffc10c750874eaf6eb2203
Unknown <gabingirardeau44@gmail.com>
2017-12-21 16:39:13 +0100

    manger les pomme

--- js/snake.js
+++ js/snake.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
     // Initialisation position Icone Econocom
     var ex = 10;
     var ey = 10;
-    var serpent =[10,10];
+    var serpent = [ex, ey];
     // Initialisation position Cercle
     var cf = 2;
     var cg = 0;
@@ -18,46 +18,44 @@ $(document).ready(function () {
     var coordonneesPommes = [];
     loadFruit();
 
-    function loadFruit (){
+    function loadFruit() {
         var cSize = cf + 1;
         var newCx;
         var newCy;
         newCx = getNombreAleatoire(cSize, (canvasWidth - cSize));
         newCy = getNombreAleatoire(cSize, (canvasHeight - cSize));
-        if(coordonneesPommes.length == 10){
+        if (coordonneesPommes.length == 10) {
             return true;
-        }else if(coordonneesPommes.length == 0){
+        } else if (coordonneesPommes.length == 0) {
             var col = false;
-        }else{
+        } else {
             var col = false;
             for (j = 0; j < coordonneesPommes.length; j++) {
                 var pomme = coordonneesPommes[j];
-                testPommeValide = collision(pomme,newCx,newCy,5);
-                if(testPommeValide){
+                testPommeValide = collision(pomme, newCx, newCy, 10);
+                if (testPommeValide) {
                     col = true;
                 }
-            
-        
             }
         }
-        var colS = collision (serpent,newCx,newCy,20);
-        if (colS){
-            col=true;
+        var colS = collision(serpent, newCx, newCy, 20);
+        if (colS) {
+            col = true;
         }
-        if(!col ){
+        if (!col) {
             coordonneesPommes.push([newCx, newCy]);
             insertCircle(newCx, newCy);
         }
         loadFruit();
     }
 
-    function collision(element,newCx,newCy,space){
+    function collision(element, newCx, newCy, space) {
         var cSize = cf + 1;
         var xInvalid = (newCx >= (element[0] - space) && newCx <= (element[0] + space + cSize));
         var yInvalid = (newCy >= (element[1] - space) && newCy <= (element[1] + space + cSize));
         return (xInvalid && yInvalid);
     }
-             var yInvalid = (newCy >= (cy - 5) && newCy <= (cy + 5 + cSize));
+
 
     $("body").keydown(function (event) {
         var keyPress = event.which;
@@ -80,7 +78,6 @@ $(document).ready(function () {
         ctx.beginPath();
         ctx.arc(cx, cy, cf, cg, 2 * Math.PI);
         ctx.stroke();
-
     }
 
     function insertIcon() {
@@ -102,9 +99,19 @@ $(document).ready(function () {
             ey += 1
         }
 
-        // else {
-        //     alert('stop');
-        // }
+        var indexToRemove;
+        for (i = 0; i < coordonneesPommes.length; i++) {
+            var pomme = coordonneesPommes[i];
+            var pommeX = pomme[0];
+            var pommeY = pomme[1];
+            var colS = collision([ex, ey], pommeX, pommeY, 3);
+            if (colS) {
+                indexToRemove = i;
+            }
+        }
+        if((typeof(indexToRemove) !== 'undefined')) {
+            coordonneesPommes.splice(indexToRemove, 1);
+        }
     };
 });
 
